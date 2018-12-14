71325dfc2e79aa40d3eb76a1e8f54d821345b931
Dimitr <graggg13@gmail.com>
2018-12-14 13:29:41 +0200

    set tag

--- models/Article.php
+++ models/Article.php
@@ -3,6 +3,7 @@
 namespace app\models;
 
 use Yii;
+use yii\helpers\ArrayHelper;
 
 /**
  * This is the model class for table "article".
@@ -67,10 +68,7 @@ class Article extends \yii\db\ActiveRecord
     /**
      * @return \yii\db\ActiveQuery
      */
-    public function getArticleTags()
-    {
-        return $this->hasMany(ArticleTag::className(), ['article_id' => 'id']);
-    }
+
 
     /**
      * @return \yii\db\ActiveQuery
@@ -111,5 +109,33 @@ class Article extends \yii\db\ActiveRecord
             return true;            
         }
     }
+    public function getTags()
+    {
+        return $this->hasMany(Tag::className(), ['id' => 'tag_id'])
+            ->viaTable('article_tag', ['article_id' => 'id']);
+    }
+     public function getSelectedTags()
+    {
+        $selectedIds = $this->getTags()->select('id')->asArray()->all();
+        return ArrayHelper::getColumn($selectedIds, 'id');
+    } 
+    public function saveTags($tags)
+    {
+        if (is_array($tags))
+        {
+            $this->clearCurrentTags();
+
+            foreach($tags as $tag_id)
+            {
+                $tag = Tag::findOne($tag_id);
+                $this->link('tags', $tag);
+            }
+        }
+    }
+    public function clearCurrentTags()
+    {
+        ArticleTag::deleteAll(['article_id'=>$this->id]);
+    }
+
 
 }
--- modules/admin/controllers/ArticleController.php
+++ modules/admin/controllers/ArticleController.php
@@ -169,4 +169,23 @@ class ArticleController extends Controller
             'categories'=>$categories
         ]);
     }
+    public function actionSetTags($id)
+    {
+        $article = $this->findModel($id);
+        $selectedTags = $article->getSelectedTags();
+        $tags = ArrayHelper::map(Tag::find()->all(), 'id', 'title');
+
+
+        if(Yii::$app->request->isPost)
+        {
+            $tags = Yii::$app->request->post('tags');
+            $article->saveTags($tags);
+            return $this->redirect(['view', 'id'=>$article->id]);
+        }
+        
+        return $this->render('tags', [
+            'selectedTags'=>$selectedTags,
+            'tags'=>$tags
+        ]); 
+    }
 }
